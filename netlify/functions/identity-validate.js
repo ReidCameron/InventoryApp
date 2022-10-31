db07f30bcd589538fd611a8e7e76a433d5c79701
Cameron Reid <reidcj@reidcj-5520.rose-hulman.edu>
2022-10-31 12:45:18 -0500

    rebuilt angular with no api calls and added logs

--- netlify/functions/identity-validate.js
+++ netlify/functions/identity-validate.js
@@ -5,17 +5,20 @@ exports.handler = async function (event, context) {
   const body = JSON.parse(event.body)
   const userID = body.user.id;
   const uri = "https://inventoryapp-reidcj.netlify.app/.netlify/functions/server/api/v1/users"
-  console.log("--------User Name--------");
+  console.log("--------User Info--------");
+  console.log(userID);
   console.log(body.user.user_metadata.full_name)
   axios.post(uri, {
     auth_id: userID,
     full_name: body.user.user_metadata.full_name,
   }).then( res => {
+    console.log("It worked")
     return {
       statusCode: 200,
-      body: JSON.stringify({ Message: res.data.Message }),
+      body: JSON.stringify({ Message: "New User Posted" }),
     };
   }).catch( err => {
+    console.log("It broke")
     return {
       statusCode: 400,
       body: JSON.stringify({ Error: "User Post Unsuccessful" }),
