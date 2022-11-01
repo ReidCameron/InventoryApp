9d042af60e1698720a040ad66196450ea7d78281
Cameron Reid <reidcj@reidcj-5520.rose-hulman.edu>
2022-11-01 09:09:49 -0500

    axios debug

--- netlify/functions/identity-validate.js
+++ netlify/functions/identity-validate.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require('axios').default;
 
 exports.handler = async function (event, context) {
   // const { identity, user } = context.clientContext;
